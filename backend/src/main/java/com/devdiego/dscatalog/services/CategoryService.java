0078c72ab6c28ea9af9056160427d3dd9b9526ef
Diego <diegorodalmeida@gmail.com>
2022-09-23 15:21:45 -0300

    Pagination

--- backend/src/main/java/com/devdiego/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devdiego/dscatalog/services/CategoryService.java
@@ -2,17 +2,21 @@ package com.devdiego.dscatalog.services;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devdiego.dscatalog.dto.CategoryDTO;
 import com.devdiego.dscatalog.entities.Category;
 import com.devdiego.dscatalog.repositories.CategoryRepository;
+import com.devdiego.dscatalog.services.exceptions.DatabaseException;
 import com.devdiego.dscatalog.services.exceptions.ResourceNotFoundException;
 
 
@@ -23,10 +27,9 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll(){
-		List <Category> list = repository.findAll();
-		
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+		Page <Category> list = repository.findAll(pageRequest);
+		return list.map(x -> new CategoryDTO(x));
 	}
 	
 	@Transactional(readOnly = true)
@@ -54,6 +57,18 @@ public class CategoryService {
 		}
 		catch(EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found" + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 }
